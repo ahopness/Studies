205c18b3abb21e6fcbd6c206fb991be00708fb30
Lucas Ângelo C. da Silva <dev.lucas.angelo@gmail.com>
2024-05-14 19:41:41 +0000

    ap5_q1

---
+++ ap5_q1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void trocar( int *var1, int *var2)
+{
+    int temp = *var1;
+    *var1 = *var2;
+    *var2 = temp;
+}
+
+int main(){
+    int n1, n2, n3;
+    scanf ( "%d %d %d", &n1, &n2, &n3 );
+
+    while ( n1 > n2 || n2 > n3 || n1 > n3)
+    {
+        if ( n1 > n2 )
+            trocar ( &n1, &n2 );
+        if ( n2 > n3 )
+            trocar ( &n2, &n3 );
+        if ( n1 > n2 )
+            trocar ( &n1, &n2 );
+    }
+
+    printf ( "%d <= %d <= %d\n", n1, n2, n3 );
+    
+    return 0;
+}
